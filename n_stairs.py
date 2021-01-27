4affb86c2b9d78bd2bf37b3018cd8ad6067a80cf
hgithub1234 <15653107+hgithub1234@users.noreply.github.com>
2021-01-27 19:19:14 +0530

    Create n_stairs.py

---
+++ n_stairs.py
@@ -0,0 +1,28 @@
+'''We print the combination of steps one can take to climb n stairs.'''
+
+import copy
+combinations = {
+	0: [[0]],
+	1: [[1]]
+}
+
+def add_num_to_dict(num):
+	global combinations
+	running_list = []
+	for i in range(1, num+1):
+		for combos in combinations[num-i]:
+			for p in combos:
+				running_list.append([i,p])
+	combinations[num] = [running_list]
+
+num_of_stairs = int(input("Enter the number of stairs: "))
+print("\n")
+
+for num in range(2, num_of_stairs+1):
+	add_num_to_dict(num)
+
+for key in combinations.keys():
+	print("---- Combinations for ", key, " Stairs are---------")
+	for i in combinations[key]: 
+		print(i)
+		print("Total Combinations: ", len(i), "\n")
