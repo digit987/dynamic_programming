0be481df490f8767ba00c644769f1036c3fb963d
hgithub1234 <15653107+hgithub1234@users.noreply.github.com>
2021-05-02 17:39:37 +0530

    Update n_parantheses_combination.py

--- n_parantheses_combination.py
+++ n_parantheses_combination.py
@@ -1,4 +1,4 @@
-'''We print the unique combinations n number of parantheses can have.
+'''We print the unique combinations n number of parentheses can have.
 For example,
 Input: 1
 Output: ()
@@ -10,15 +10,15 @@ Output: ((())), ()()(), (())(), (()()), ()(())
 
 '''We initialize the dictionary with base case 1'''
 
-parantheses_combinations = {
+parentheses_combinations = {
 	1: ['()']
 }
 
-def add_parantheses_combination_to_dict(parantheses):
-	global parantheses_combinations
+def add_parentheses_combination_to_dict(parentheses):
+	global parentheses_combinations
 	new_combination_list = []
-	previous_parantheses_combination = parantheses_combinations[parantheses-1]
-	for combination in previous_parantheses_combination:
+	previous_parentheses_combination = parentheses_combinations[parentheses-1]
+	for combination in previous_parentheses_combination:
 		new_combination_list.append('()' + combination)
 		for char_position_in_combination in range(len(combination)):
 			if char_position_in_combination == len(combination) - 1:
@@ -26,25 +26,25 @@ def add_parantheses_combination_to_dict(parantheses):
 			else:
 				new_combination_list.append(combination[0:char_position_in_combination+1] + '()' + combination[char_position_in_combination+1:len(combination)])
 			new_combination_list = list(set(new_combination_list))
-	parantheses_combinations[parantheses] = new_combination_list
+	parentheses_combinations[parentheses] = new_combination_list
 
-num_of_parantheses = int(input("Enter the number of parantheses: "))
-if num_of_parantheses <= 0:
+num_of_parentheses = int(input("Enter the number of parentheses: "))
+if num_of_parentheses <= 0:
 	print("Please Enter a natural number")
 	exit()
 print("\n")
 
-for parantheses in range(2, num_of_parantheses+1):
-	add_parantheses_combination_to_dict(parantheses)
+for parentheses in range(2, num_of_parentheses+1):
+	add_parentheses_combination_to_dict(parentheses)
 
 catalan_number = 0
 
-for key in parantheses_combinations.keys():
-	print("---- Combinations for Parantheses", key, " are---------")
-	for combination in parantheses_combinations[key]: 
+for key in parentheses_combinations.keys():
+	print("---- Combinations for Parentheses", key, " are---------")
+	for combination in parentheses_combinations[key]: 
 		print(combination)
-	print("Total Combinations: ", len(parantheses_combinations[key]), "\n")
-	if key == num_of_parantheses:
-		catalan_number = len(parantheses_combinations[key])
+	print("Total Combinations: ", len(parentheses_combinations[key]), "\n")
+	if key == num_of_parentheses:
+		catalan_number = len(parentheses_combinations[key])
 		
-print("Catalan number for ",  num_of_parantheses, " is: ", catalan_number)
+print("Catalan number for ",  num_of_parentheses, " is: ", catalan_number)
